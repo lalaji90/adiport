c8fe97c3f4b8bfc8120d62e0e0a46758484c817c
lalaji90 <156190163+lalaji90@users.noreply.github.com>
2024-02-28 13:14:45 +0530

    added index.html

---
+++ index.html/script.js
@@ -0,0 +1,21 @@
+overlay.addEventListener("click", closeMobileNav);
+
+contactForm.addEventListener('submit', (event) => {
+  // event.preventDefault();
+  alert("your response has been successfully submitted")}
+
+  );
+
+
+var typeData = new Typed(".role", {
+    strings: [
+      "Web Developer",
+      "Frontend Developer",
+      "Full Stack Developer",
+      "Coder",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1000,
+  });
